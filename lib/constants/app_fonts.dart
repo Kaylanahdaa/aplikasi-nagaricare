8b14592904a9d93ba31a748036e570e942d80d76
Khalilul Afwan <khalilulafwan@student.unp.ac.id>
2024-10-09 11:36:40 +0700

    bikin constant untuk tema aplikasi

---
+++ lib/constants/app_fonts.dart
@@ -0,0 +1,8 @@
+class AppFonts {
+  static const String primaryFont = 'Poppins';
+  static const String secondaryFont = 'OpenSans';
+
+  static const double headingFontSize = 24.0;
+  static const double bodyFontSize = 16.0;
+  static const double smallFontSize = 12.0;
+}

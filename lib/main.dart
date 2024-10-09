fb90616cedc164afc1657249bde2fdcd673a4359
Khalilul Afwan <khalilulafwan@student.unp.ac.id>
2024-10-09 16:05:14 +0700

    test bottom navbar with GetX

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:aplikasi_nagaricare/screens/help_screen.dart';
+// import 'package:aplikasi_nagaricare/screens/help_screen.dart';
+import 'package:aplikasi_nagaricare/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: HelpScreenWidget(),
+      home: MainScreen(),
     );
   }
 }

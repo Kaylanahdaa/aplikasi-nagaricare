4c4e431492191c5ef0aa5fc3495124132723465e
Khalilul Afwan <khalilulafwan@student.unp.ac.id>
2024-10-14 09:20:16 +0700

    implementasi getx pada halaman utama (home)

---
+++ lib/controllers/home_controller.dart
@@ -0,0 +1,8 @@
+// Controller for managing any state (add reactive state if necessary)
+import 'package:get/get.dart';
+
+class HomePageController extends GetxController {
+  // You can add reactive variables here if needed, for example:
+  // var topics = [].obs;
+  // var posts = [].obs;
+}

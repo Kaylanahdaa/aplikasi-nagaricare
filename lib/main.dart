9b134c3f0465b509754f92b0d0ee471f53fcad3b
Kayla Nahda <kayla_nahda@icloud.com>
2024-10-16 09:46:21 +0700

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:aplikasi_nagaricare/screens/auth/welcome/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:aplikasi_nagaricare/screens/auth/welcome/welcome_screen.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
 }
 

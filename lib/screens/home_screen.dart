18a02396192bd657d55b818d6f6db318a00165eb
Khalilul Afwan <khalilulafwan@student.unp.ac.id>
2024-10-23 08:37:40 +0700

    remove unused import + fix posts screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:aplikasi_nagaricare/constants/app_colors.dart';
-import 'package:aplikasi_nagaricare/screens/forum/post_screen.dart';
+// import 'package:aplikasi_nagaricare/screens/forum/post_screen.dart';
 import 'package:aplikasi_nagaricare/widgets/carousel_menu.dart';
 import 'package:aplikasi_nagaricare/widgets/post_forum.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +8,8 @@ import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 // import '/widgets/posts.dart';
 import 'package:ionicons/ionicons.dart';
+
+import '../widgets/posts.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -101,134 +103,7 @@ class HomePage extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Column(
-                    children: controller.posts
-                        .map((post) => GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (_) => PostScreen(
-                                              idPosts: post
-                                                  .idPosts, // Assuming PostScreen expects idPosts.
-                                            )));
-                              },
-                              child: Container(
-                                height: 180,
-                                margin: const EdgeInsets.all(15.0),
-                                decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(10.0),
-                                    boxShadow: [
-                                      BoxShadow(
-                                          color:
-                                              Colors.black26.withOpacity(0.05),
-                                          offset: const Offset(0.0, 6.0),
-                                          blurRadius: 10.0,
-                                          spreadRadius: 0.10)
-                                    ]),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(15.0),
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: <Widget>[
-                                      SizedBox(
-                                        height: 70,
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: <Widget>[
-                                            Row(
-                                              children: <Widget>[
-                                                Padding(
-                                                  padding:
-                                                      const EdgeInsets.only(
-                                                          left: 8.0),
-                                                  child: Column(
-                                                    crossAxisAlignment:
-                                                        CrossAxisAlignment
-                                                            .start,
-                                                    mainAxisAlignment:
-                                                        MainAxisAlignment
-                                                            .center,
-                                                    children: <Widget>[
-                                                      SizedBox(
-                                                        width: MediaQuery.of(
-                                                                    context)
-                                                                .size
-                                                                .width *
-                                                            0.65,
-                                                        child: Text(
-                                                          post.title,
-                                                          style: const TextStyle(
-                                                              overflow:
-                                                                  TextOverflow
-                                                                      .ellipsis,
-                                                              fontSize: 18,
-                                                              fontWeight:
-                                                                  FontWeight
-                                                                      .bold,
-                                                              letterSpacing:
-                                                                  .4),
-                                                        ),
-                                                      ),
-                                                      const SizedBox(
-                                                          height: 2.0),
-                                                      Row(
-                                                        children: <Widget>[
-                                                          Text(
-                                                            post.email, // Displaying post author email.
-                                                            style: TextStyle(
-                                                                color: Colors
-                                                                    .red
-                                                                    .withOpacity(
-                                                                        0.6)),
-                                                          ),
-                                                          const SizedBox(
-                                                              width: 15),
-                                                          Text(
-                                                            post.createdAt, // Formatted date.
-                                                            style: TextStyle(
-                                                                color: Colors
-                                                                    .grey
-                                                                    .withOpacity(
-                                                                        0.6)),
-                                                          )
-                                                        ],
-                                                      )
-                                                    ],
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        height: 50,
-                                        child: Center(
-                                          child: Text(
-                                            post.content.length > 80
-                                                ? "${post.content.substring(0, 80)}.."
-                                                : post.content,
-                                            style: TextStyle(
-                                              color:
-                                                  Colors.grey.withOpacity(0.8),
-                                              fontSize: 16,
-                                              letterSpacing: .3,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                      const SizedBox(height: 5),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ))
-                        .toList(),
-                  )
+                  Posts(),
                 ],
               ),
             ),
--- lib/widgets/post_to_forum.dart
+++ lib/widgets/post_to_forum.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-import 'package:aplikasi_nagaricare/constants/app_colors.dart';
+// import 'package:aplikasi_nagaricare/constants/app_colors.dart';
 import 'package:aplikasi_nagaricare/controllers/post_forum_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';

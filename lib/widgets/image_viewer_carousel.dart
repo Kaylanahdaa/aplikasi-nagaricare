b43f53c9aeea4a190c2cfe7dd7478a7553ce4a77
Khalilul Afwan <khalilulafwan@student.unp.ac.id>
2024-10-29 10:59:57 +0700

    update showdialog

--- lib/widgets/image_viewer_carousel.dart
+++ lib/widgets/image_viewer_carousel.dart
@@ -20,8 +20,14 @@ class CustomImageViewer {
                   'Berita Bank Nagari',
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-                content: Text(
-                    "Non est Lorem est consequat et id aliqua ad aute nisi deserunt."),
+                content: Container(
+                  height: 300,
+                  width: 300,
+                  child: SingleChildScrollView(
+                    child: Text(
+                        "Non est Lorem est consequat et id aliqua ad aute nisi deserunt. Mollit ut sit voluptate non tempor. Ullamco sit aliqua quis laborum ullamco esse. Amet cupidatat est nostrud et magna laborum. Non deserunt amet duis non eiusmod veniam fugiat esse. Lorem aliqua amet magna anim id magna dolore duis exercitation elit. Anim consequat voluptate ad nostrud ipsum aliquip adipisicing eiusmod eu nisi. Mollit est ipsum pariatur consectetur occaecat cillum ex. Et Lorem do in in commodo minim consectetur consequat magna minim. Officia id aliqua aliqua cillum velit deserunt non ipsum. Aliquip sit do consectetur esse proident irure nostrud sunt in consectetur anim exercitation. Cupidatat quis nostrud id sunt est exercitation Lorem quis voluptate voluptate. Laborum esse commodo est ea occaecat id ad sint ullamco incididunt anim excepteur. Nisi culpa ex sunt irure in cupidatat minim velit aliqua in non officia. Eu commodo proident labore cupidatat magna occaecat proident culpa in commodo ullamco. Ut irure dolor eiusmod aliquip ut dolore fugiat eu ullamco qui consequat dolore."),
+                  ),
+                ),
                 actions: [
                   TextButton(
                     onPressed: () {

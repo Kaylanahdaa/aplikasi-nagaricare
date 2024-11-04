a7d036e1a80eef505d36a277fc7ae8281bfc4fa6
Khalilul Afwan <khalilulafwan@student.unp.ac.id>
2024-11-04 09:18:55 +0700

    ganti image

--- lib/models/image_carousel.dart
+++ lib/models/image_carousel.dart
@@ -1,12 +1,38 @@
+// class AppData {
+//   AppData._();
+//   static final List<String> outerStyleImages = [
+//     'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/new-arrival-discount-offer-on-shoes-poster-ad-design-template-20e8be063593e460ec1eadf156df2a71_screen.jpg?ts=1607504280',
+//     'https://www.mall499.com/wp-content/uploads/2021/12/banner-skechers-1.png',
+//     'https://www.mall499.com/wp-content/uploads/2021/12/banner-skechers-1.png',
+//     'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/shoes-craze-sale-facebook-ad-design-template-b1d1738fd5e9e0f6e3152ec502a1c2e1_screen.jpg?ts=1567579016',
+//     'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/shoes-craze-sale-facebook-ad-design-template-b1d1738fd5e9e0f6e3152ec502a1c2e1_screen.jpg?ts=1567579016',
+//     'https://rstatic.shoecarnival.com/domain/5265_508_20231225_Winter_Season_Store_Locator_Banner2_(1).jpg',
+//     'https://graphicsfamily.com/wp-content/uploads/2020/07/Shoes-Advertising-Banner-Design-Template-scaled.jpg',
+//   ];
+// }
+
+// class AppData {
+//   AppData._();
+//   static final List<String> outerStyleImages = [
+//     'http://banknagari.co.id/Assets/temp_/image1730099934833.jpg',
+//     'https://banknagari.co.id/Assets/temp_/image1730099527401.jpg',
+//     'https://banknagari.co.id/Assets/temp_/image1728622492120.jpg',
+//     'https://banknagari.co.id/Assets/temp_/image1716178303421.jpg',
+//     'https://banknagari.co.id/Assets/temp_/image1714443757669.png',
+//     'https://banknagari.co.id/Assets/temp_/image1725593068432.jpg',
+//     'https://banknagari.co.id/Assets/temp_/image1725593300367.jpg',
+//   ];
+// }
+
 class AppData {
   AppData._();
   static final List<String> outerStyleImages = [
-    'https://banknagari.co.id/Assets/temp_/image1730099934833.jpg',
-    'https://banknagari.co.id/Assets/temp_/image1730099527401.jpg',
-    'https://banknagari.co.id/Assets/temp_/image1728622492120.jpg',
-    'https://banknagari.co.id/Assets/temp_/image1716178303421.jpg',
-    'https://banknagari.co.id/Assets/temp_/image1714443757669.png',
-    'https://banknagari.co.id/Assets/temp_/image1725593068432.jpg',
-    'https://banknagari.co.id/Assets/temp_/image1725593300367.jpg',
+    'https://www.bankmandiri.co.id/documents/20143/175704414/RESIZE-%281440-X-548%29---JET.jpg/35103317-8704-b3be-c294-c44c7308f44a?t=1710813791599',
+    'https://www.bankmandiri.co.id/documents/20143/175704414/RESIZE-%281440-X-548%29---AIRPORT-LOUNGE.jpg/b6dcc466-a967-a22b-a859-6eeb04b317f6?t=1710813892833',
+    'https://www.bankmandiri.co.id/documents/20143/175704414/RESIZE-%281440-X-548%29---AIRPORT-TRANSFER.jpg/4da42c7c-92dd-90c2-42eb-61ddbec45f44?t=1710813977654',
+    'https://www.bankmandiri.co.id/documents/20143/185344218/RESIZE-%281440-X-548%29---MGM-WEALTH.jpg/88d52692-868e-46f4-8164-ccb263befcfe?t=1710832837136',
+    'https://www.bankmandiri.co.id/documents/20143/269721802/RESIZE+BANNER+BESAR+%281440+X+548%29+-+WEL+GIFT.jpg/5383d7d5-c3be-3147-065d-a48d49cf7d5f?t=1720581311982',
+    'https://www.bankmandiri.co.id/documents/20143/185344218/RESIZE-%281440-X-548%29---GOLF-CLINIC.jpg/38e267ee-26ea-f45c-2690-548893d0e558?t=1710832832976',
+    'https://www.bankmandiri.co.id/documents/20143/244081507/thai-debitcontactless.png/ce7d4b66-4336-004b-3563-ed85056cda6d?t=1730449774490',
   ];
 }
